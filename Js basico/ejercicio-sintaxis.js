ef804198a0595589851569fb6191ec94b3cb72cc
Gise <100255174+GiseRoman@users.noreply.github.com>
2022-10-17 12:41:43 -0500

    Update and rename ejercicio-sintaxis to ejercicio-sintaxis.js

---
+++ Js basico/ejercicio-sintaxis.js
@@ -0,0 +1,12 @@
+const lista = [
+      "Gisela",
+      31,
+      true,
+      new Date(1991, 9, 11),
+     {
+        titulo: "Sss class suicide hunter",
+        autor: "Shin Noah",
+        fecha: "Noviembre 20 de 2018",
+        url: "https://page.kakao.com/content/52822434"
+     },
+];
